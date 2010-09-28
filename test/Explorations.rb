5a7710479a780f6e7e0d5e8f40cfa1383d49b1c2
Raul Jara <rjara1@nd.edu>
2010-09-28 10:34:17 -0400

    Not sure how long it's been since the last commit.  Apparently not long.

--- test/Explorations.rb
+++ test/Explorations.rb
@@ -5,4 +5,4 @@ require File.dirname(__FILE__) + '/StataTableParserHelper.rb'
 include StataTableParserHelper
 
 @parser = StataTableParser.new(File.dirname(__FILE__) + "/../TestTables/SmallWideTable.txt")
-@parser.xml_out(File.dirname(__FILE__) + "/../TestTables/SmallWideTableBasicOut.xml")
+@parser.xml_out(File.dirname(__FILE__) + "/../TestTables/SmallWideTableBasicOut.xml", :row_totals => true)
